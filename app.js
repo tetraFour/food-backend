3da216228a4e1a05971479890af0e98cf30bd96e
tetraFour <a.korensky@mail.ru>
2021-02-25 18:56:01 +0300

    rewrite CORS

--- app.js
+++ app.js
@@ -29,16 +29,27 @@ app.set('port', process.env.PORT || 7000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+  );
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: 'https://food-admin-client.herokuapp.com',
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: 'https://food-admin-client.herokuapp.com',
+//     credentials: true,
+//   }),
+// );
 app.use(
   session({
     secret: 'TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX',
